a7d596769d3b44dacfeea2795b7fa90c8c85fedb
Anthony Venturini <anthonydventurini@gmail.com>
2019-03-06 18:01:06 -0600

    initial commit

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { fetchChars } from "../actions"
+
 // import actions
 
 class CharacterListView extends React.Component {
@@ -27,9 +29,4 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
-export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
-)(CharacterListView);
+export default connect(mapStateToProps, {fetchChars})(CharacterListView);
